9d330530b86c748872fa1dd2f34b14d0364c5842
Han034 <mehmeteminliyiz@gmail.com>
2025-01-08 01:27:01 +0300

    Adım 7: Controller'ların Oluşturulması

---
+++ Presentation/GeminiAdvancedAPI/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using GeminiAdvancedAPI.Application.Features.Product.Commands.CreateProduct;
+using GeminiAdvancedAPI.Application.Features.Product.Commands.DeleteProduct;
+using GeminiAdvancedAPI.Application.Features.Product.Commands.UpdateProduct;
+using GeminiAdvancedAPI.Application.Features.Product.Dtos;
+using GeminiAdvancedAPI.Application.Features.Product.Queries.GetProduct;
+using GeminiAdvancedAPI.Application.Features.Product.Queries.GetProducts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeminiAdvancedAPI.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public ProductController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<ProductDto>> Get(Guid id)
+		{
+			var query = new GetProductQuery(id);
+			var product = await _mediator.Send(query);
+			return Ok(product);
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<ProductDto>>> GetList()
+		{
+			var query = new GetProductsQuery();
+			var products = await _mediator.Send(query);
+			return Ok(products);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Guid>> Create([FromBody] CreateProductCommand command)
+		{
+			var id = await _mediator.Send(command);
+			return Ok(id);
+		}
+
+		[HttpPut]
+		public async Task<ActionResult> Update([FromBody] UpdateProductCommand command)
+		{
+			await _mediator.Send(command);
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> Delete(Guid id)
+		{
+			var command = new DeleteProductCommand(id);
+			await _mediator.Send(command);
+			return NoContent();
+		}
+	}
+}
